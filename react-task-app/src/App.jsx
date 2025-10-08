56119fca107e8a583fb7d1ebecf337b5a9cf7b01
Vasuki t <iamvasukithangaraj@gmail.com>
2025-10-08 20:48:19 +0530

    initial commit

---
+++ react-task-app/src/App.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="p-6 bg-gray-100 min-h-screen">
+      <h1 className="text-2xl font-bold mb-6 text-center">
+        Task Management App
+      </h1>
+
+      <form className="bg-white p-4 rounded shadow-md mx-auto max-w-md">
+        <label className="font-semibold mb-2">Task Title</label>
+        <input type="text" className="border p-2 mb-2 w-full " />
+
+        <label className="font-semibold mb-2">Task Description</label>
+        <textarea name="" className="border p-2 mb-2 w-full "></textarea>
+
+        <label className="font-semibold mb-2">Assigned</label>
+        <input type="text" className="border p-2 mb-2 w-full " />
+
+        <label className="font-semibold mb-2">Due Date</label>
+        <input type="date" className="border p-2 mb-2 w-full " />
+
+        <label className="block mb-3">
+        <input type="checkbox" className="mr-2 " />Completed</label>
+
+        <button className="bg-violet-500 text-white px-4 py-2 rounded  mr-2 w-full">submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
+
+// http://localhost:5000/tasks
